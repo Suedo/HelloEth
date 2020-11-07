be879d95d46cf25d930860963c4a039590fe3d31
Suedo <somjitnag192@gmail.com>
2020-11-07 17:47:07 +0530

    reformat

--- NodeGanache/src/contracts/SharedWallet.sol
+++ NodeGanache/src/contracts/SharedWallet.sol
@@ -4,25 +4,19 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 import {Allowance} from "Allowance.sol";
 
 contract SharedWallet is Allowance {
-    event MoneySent(address indexed _beneficiary, uint256 _amount);
-    event MoneyReceived(address indexed _from, uint256 _amount);
+  event MoneySent(address indexed _beneficiary, uint256 _amount);
+  event MoneyReceived(address indexed _from, uint256 _amount);
 
-    function withdrawMoney(address payable _to, uint256 _amount)
-        public
-        ownerOrAllowed(_amount)
-    {
-        require(
-            _amount <= address(this).balance,
-            "Contract doesn't own enough money"
-        );
-        if (!isOwner()) {
-            reduceAllowance(msg.sender, _amount);
-        }
-        emit MoneySent(_to, _amount);
-        _to.transfer(_amount);
+  function withdrawMoney(address payable _to, uint256 _amount) public ownerOrAllowed(_amount) {
+    require(_amount <= address(this).balance, "Contract doesn't own enough money");
+    if (!isOwner()) {
+      reduceAllowance(msg.sender, _amount);
     }
+    emit MoneySent(_to, _amount);
+    _to.transfer(_amount);
+  }
 
-    receive() external payable {
-        emit MoneyReceived(msg.sender, msg.value);
-    }
+  receive() external payable {
+    emit MoneyReceived(msg.sender, msg.value);
+  }
 }
--- NodeGanache/src/contracts/hello.sol
+++ NodeGanache/src/contracts/hello.sol
@@ -1,33 +1,33 @@
 pragma solidity ^0.7.0;
 
 contract HelloBlock {
-    string myString;
-    mapping(address => uint256) balances;
-    event Received(address, uint256, uint256); // print details for received amount
-    event Sent(address, address, uint256, uint256); // print details regarding amount transferred
+  string myString;
+  mapping(address => uint256) balances;
+  event Received(address, uint256, uint256); // print details for received amount
+  event Sent(address, address, uint256, uint256); // print details regarding amount transferred
 
-    function setMyString(string memory _myString) public {
-        myString = _myString;
-    }
+  function setMyString(string memory _myString) public {
+    myString = _myString;
+  }
 
-    function getMyString() public view returns (string memory) {
-        return myString;
-    }
+  function getMyString() public view returns (string memory) {
+    return myString;
+  }
 
-    function withdrawEther(
-        address _from,
-        address payable _to,
-        uint256 _amount
-    ) public {
-        if (balances[_from] >= _amount) {
-            _to.transfer(_amount);
-            emit Sent(_from, _to, _amount, balances[_from]);
-        }
+  function withdrawEther(
+    address _from,
+    address payable _to,
+    uint256 _amount
+  ) public {
+    if (balances[_from] >= _amount) {
+      _to.transfer(_amount);
+      emit Sent(_from, _to, _amount, balances[_from]);
     }
+  }
 
-    // default fallback
-    receive() external payable {
-        balances[msg.sender] += msg.value;
-        emit Received(msg.sender, msg.value, balances[msg.sender]);
-    }
+  // default fallback
+  receive() external payable {
+    balances[msg.sender] += msg.value;
+    emit Received(msg.sender, msg.value, balances[msg.sender]);
+  }
 }
