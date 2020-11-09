14ecca6a5cf05f01169eb941275168524635cd88
Suedo <somjitnag192@gmail.com>
2020-11-09 12:03:59 +0530

    vscode + prettier

--- helloTruffle/contracts/CrowdFundWithDeadline.sol
+++ helloTruffle/contracts/CrowdFundWithDeadline.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.7.4;
 pragma experimental ABIEncoderV2;
 
 contract CrowdFundingWithDeadline {
-  enum Status {Ongoing, Failed, Succeeded, PaidOut} // internally recorded as 0,1,2,3
+  enum Status { Ongoing, Failed, Succeeded, PaidOut } // internally recorded as 0,1,2,3
 
   string public name;
   uint256 public targetAmount;
